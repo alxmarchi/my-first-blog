b48d21b9d8c4f6450af4fafc5df1266d4b72cee6
zyex <zyex@mail.ru>
2019-08-02 20:34:26 +0300

    new commit

--- blog/models.py
+++ blog/models.py
@@ -1,18 +1,33 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.shortcuts import reverse
 
 
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    title = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    text = models.TextField(blank=True, db_index=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    created_date = models.DateTimeField(auto_now_add=True)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    def get_absolute_url(self):
+        return reverse('post_detail_url', kwargs={'slug': self.slug})
+
+  #  def publish(self):
+   #     self.published_date = timezone.now()
+    #    self.save()
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def get_absolute_url(self):
+        return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def __str__(self):
+        return '{}'.format(self.title)
